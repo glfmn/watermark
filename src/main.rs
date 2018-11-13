8f80054b079b2594c548ebd63406e5ff64b1c6f7
Gwen Lofman <Gwen@Lofman.io>
2018-11-12 19:34:26 -0500

    Implement k-means clustering

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #[macro_use]
 extern crate clap;
 extern crate image;
+extern crate rayon;
+extern crate rand;
 
 use image::{GenericImageView, GenericImage};
+use rayon::prelude::*;
+use rand::prelude::*;
+use std::hash::{Hash, Hasher};
 
 /// A block of 16x16 grayscale pixel data
 #[derive(Clone)]
@@ -28,9 +33,7 @@ impl Block {
         debug_assert!(w.wrapping_rem(16) == 0);
         debug_assert!(h.wrapping_rem(16) == 0);
 
-        print!("{}x{}: ", w, h);
         let (w, h) = (w/16, h/16);
-        println!("{}x{}", w, h);
         let mut blocks = Vec::with_capacity((w*h) as usize);
 
         for r in (0..h).map(|r| r * 16) {
@@ -46,6 +49,29 @@ impl Block {
         blocks
     }
 
+    /// Create a new block which is the mean of a set of blocks
+    pub fn as_mean<'a, I>(blocks: &'a mut I) -> Self
+    where
+        I: Iterator<Item=&'a Block>
+    {
+        let mut mean = [0; 256];
+        let mut data = [0; 256];
+
+        let mut n: usize = 0;
+        for block in blocks {
+            for i in 0..mean.len() {
+                mean[i] += block.data[i] as usize;
+            }
+            n += 1;
+        }
+
+        for i in 0..data.len() {
+            data[i] = (mean[i]/n) as u8;
+        }
+
+        Block {x: 0, y: 0, data}
+    }
+
     /// Simple euclidiand distance between two slices
     pub fn distance(&self, other: &Block) -> f64 {
         let mut sum = 0.;
@@ -53,8 +79,8 @@ impl Block {
             let a = self.data[i];
             let b = other.data[i];
             // Subtract preventing underflow
-            let diff = a.max(b) - a.min(b);
-            sum += (diff * diff) as f64;
+            let diff = (a.max(b) - a.min(b)) as f64;
+            sum += diff * diff;
         }
 
         sum.sqrt()
@@ -78,6 +104,16 @@ impl PartialEq for Block {
 }
 
 impl Eq for Block {}
+
+impl Hash for Block {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+        for d in 0..self.data.len() {
+            self.data[d].hash(state);
+        }
+    }
+}
 
 #[cfg(test)]
 mod block_test {
@@ -107,10 +143,78 @@ mod block_test {
                 // If any value is not 1, it did not come from the image,
                 // which implies that the implementation at least fills the
                 // data buffer with information from the image
-                assert!(d == &1)
+                assert!(*d == 1)
             }
         }
     }
+}
+
+fn kmeans(k: usize, tol: f64, blocks: &Vec<Block>)
+    -> std::collections::HashMap<Block, std::vec::Vec<usize>>
+{
+    use std::collections::HashMap;
+
+    let mut centroids: Vec<Block> = Vec::with_capacity(k);
+    let n = blocks.len();
+
+    // Get k data blocks as initial centroids
+    let mut m = k;
+    for i in 0..n {
+        // select m of remaining n-i
+        if (random::<usize>() % (n-i)) < m {
+            centroids.push(blocks[i].clone());
+            m -= 1;
+        }
+    }
+
+    let mut clusters: HashMap<usize, Vec<usize>> = HashMap::new();
+
+    let mut err = std::f64::MAX;
+    while err > tol {
+        // Concurrently calculate the closest centroid to each block
+        let best: Vec<usize> = blocks.par_iter()
+            .map(|ref b| {
+                centroids.iter()
+                    .map(|ref c| b.distance(c))
+                    .enumerate()
+                    .fold((0, std::f64::MAX), |acc, (i, d)| {
+                        if d < acc.1 {
+                            (i, d)
+                        } else {
+                            acc
+                        }
+                    })
+            })
+            .map(|(c, _)| c)
+            .collect();
+
+        clusters = HashMap::new();
+        for (block, &centroid) in best.iter().enumerate() {
+            let c = clusters.entry(centroid).or_insert(Vec::new());
+            c.push(block);
+        }
+
+        // Concurrently calculate means
+        let new: Vec<Block> = centroids.par_iter()
+            .enumerate()
+            .map(|(k, _)| {
+                Block::as_mean(&mut clusters[&k].iter().map(|&b| &blocks[b]))
+            })
+            .collect();
+
+        // Error as sum of the distance between the new and old centroids
+        err = centroids.iter()
+            .zip(new.iter())
+            .fold(0., |sum, (o, n)| sum + n.distance(o));
+        centroids = new;
+    }
+
+    let mut k_means = HashMap::new();
+    for (k, centroid) in centroids.iter().enumerate() {
+        k_means.insert(centroid.clone(), clusters[&k].clone());
+    }
+
+    k_means
 }
 
 fn main() {
@@ -131,6 +235,8 @@ fn main() {
     let _mark = image::open(mark).expect("Unable to open watermark").to_luma();
 
     let blocks = Block::from_image(&mut image);
+
+    let _ = kmeans(20, 1e-6, &blocks);
 
     println!("{}", output);
 }
